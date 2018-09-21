8613ffbd4f9eab1c067ea7e3ad8307751b2fd722
saboth <43369061+saboth@users.noreply.github.com>
2018-09-22 00:38:48 +0200

    Updating the first two solutions for the Eloquent Javascript exercises.

---
+++ Exercise_1.js
@@ -0,0 +1,26 @@
+/*
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.
+*/
+
+let hash = '#';
+let i;
+
+for (i = 0; i < 7; i++) {
+    console.log(hash);
+    hash = hash + '#';
+}
